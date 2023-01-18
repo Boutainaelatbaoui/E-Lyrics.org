10e80d87611f20905265502b95cac1a1664daf24
Boutaina <boutaina.elatbaoui@gmail.com>
2023-01-18 10:01:29 +0100

    Added statistics page

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ function addSong(){
 }
 
 function editSong(){
-    document.getElementById('product-save-btn').style.display   = "none";
-    document.getElementById('product-update-btn').style.display = "block";
-    document.getElementById('product-delete-btn').style.display = "block";
+    document.getElementById('song-save-btn').style.display   = "none";
+    document.getElementById('song-update-btn').style.display = "block";
+    document.getElementById('song-delete-btn').style.display = "block";
 }
---
+++ statistics.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    $title = "Dashboard | E-Lyrics";
+    include("include/head.php");
+    ?>
+<body class="dash-body">
+    <!-- BEGIN navbar -->
+    <?php
+        include("include/navbar.php");
+    ?>
+    <!-- END navbar -->
+    <!-- BEGIN Offcanvas -->
+    <?php
+        include("include/sidebar.php");
+    ?>
+    <!-- END Offcanvas -->
+    <main class="">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 mt-5">
+                <div class="col mb-3">
+                    <div class="card card-statis">
+                        <div class="card-body d-flex flex-row justify-content-around align-items-center mt-3 mb-3">
+                            <div><i class="bi bi-people fs-2 text-center"></i></div>
+                            <div class="text-truncate">
+                                <h5 class="card-title">Total Admins</h5>
+                                <p class="card-text fs-5"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-3">
+                    <div class="card card-statis">
+                        <div class="card-body d-flex flex-row justify-content-around align-items-center mt-3 mb-3">
+                            <div><i class="bi bi-person-square fs-2 text-center"></i></div>
+                            <div class="text-truncate">
+                                <h5 class="card-title">Total Artists</h5>
+                                <p class="card-text fs-5"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-3">
+                    <div class="card card-statis">
+                        <div class="card-body d-flex flex-row justify-content-around align-items-center mt-3 mb-3">
+                            <div><i class="bi bi-blockquote-right fs-2"></i></div>
+                            <div class="text-truncate">
+                                <h5 class="card-title">Total Titles</h5>
+                                <p class="card-text fs-5"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card card-statis">
+                        <div class="card-body d-flex flex-row justify-content-around align-items-center mt-3 mb-3">
+                            <div><i class="bi bi-disc fs-2"></i></div>
+                            <div class="text-truncate">
+                                <h5 class="card-title">Total Albums</h5>
+                                <p class="card-text fs-5"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </main> 
+    <!-- ================== BEGIN core-js ================== -->
+    <?php
+        include("include/scripts.php");
+    ?>
+	<!-- ================== END core-js ================== -->
+</body>
+</html>
