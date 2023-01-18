67c1b1891c9fa70fb719e5148cdab2982a58f6e4
Boutaina <boutaina.elatbaoui@gmail.com>
2023-01-18 18:29:56 +0100

    Modified model

--- include/model.php
+++ include/model.php
@@ -9,10 +9,16 @@
 					<div class="modal-body">
 							<!-- This Input Allows Storing Task Index  -->
 							<input  type="hidden" name="song-id" id="song-id">
-							<div class="mb-3">
-								<label class="form-label">Title</label>
-								<input type="text" class="form-control" name="title" id="song-title"/>
-							</div>
+                            <div class="row mb-3">
+                                <div class="col-md-6 col-sm-12">
+									<label class="form-label">Title</label>
+									<input type="text" class="form-control" name="title" id="song-title"/>
+                                </div>
+                                <div class="col-md-6 col-sm-12">
+									<label class="form-label">Album</label>
+									<input type="text" class="form-control" name="album" id="song-album"/>
+                                </div>
+                            </div>
                             <div class="row mb-3">
                                 <div class="col-md-6 col-sm-12">
                                     <label class="form-label">Artist</label>
@@ -26,10 +32,6 @@
                                     <input type="number" class="form-control" name="year" id="song-year"/>
                                 </div>
                             </div>
-                            <div class="mb-3">
-								<label class="form-label">Album</label>
-								<input type="text" class="form-control" name="album" id="song-album"/>
-							</div>
 							<div class="mb-0">
 								<label class="form-label">Lyrics</label>
 								<textarea class="form-control" rows="8" name="lyrics" id="song-lyrics"></textarea>
