03a106c24d7a2ee3084399f39b10b4821e1aa067
Boutaina <boutaina.elatbaoui@gmail.com>
2023-01-21 13:51:20 +0100

    modified dynamic form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-let more_modal = document.querySelector('.more-modal');
+let more_modal  = document.querySelector('.more-modal');
+let more_button = document.querySelector('#song-more-btn');
+let modal_body  = document.querySelector('.modal-body');
 
 function addSong(){
     document.getElementById('song-save-btn').style.display   = "block";
@@ -20,36 +22,6 @@ function editSong(id){
     document.getElementById('song-lyrics').value = document.getElementById(id).getAttribute("lyrics");
 }
 
-function addMore() {
-    let another_modal = `
-        <input  type="hidden" name="song-id" id="song-id">
-        <div class="row mb-3">
-            <div class="col-md-6 col-sm-12">
-                <label class="form-label">Title</label>
-                <input type="text" class="form-control" name="title[]" id="song-title"/>
-            </div>
-            <div class="col-md-6 col-sm-12">
-                <label class="form-label">Album</label>
-                <input type="text" class="form-control" name="album[]" id="song-album"/>
-            </div>
-        </div>
-        <div class="row mb-3">
-            <div class="col-md-6 col-sm-12">
-                <label class="form-label">Artist</label>
-                <input type="text" class="form-control" name="artist[]" id="song-artist"/>
-            </div>
-            <div class="col-md-6 col-sm-12">
-                <label class="form-label">Year</label>
-                <input type="number" class="form-control" name="year[]" id="song-year"/>
-            </div>
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Lyrics</label>
-            <textarea class="form-control" rows="8" name="lyrics[]" id="song-lyrics"></textarea>
-        </div>
-        <div class="mb-3">
-            <a href="#" class="btn song-action-btn" id="song-more-btn" onclick="addMore()"><i class="bi bi-plus"></i> Add more</a>
-        </div>
-    `;
-    more_modal.innerHTML += another_modal;
-}
+more_button.addEventListener("click", () => {
+    more_modal.append(modal_body.cloneNode(true));
+})
--- include/model.php
+++ include/model.php
@@ -32,16 +32,14 @@
 							<div class="mb-3">
 								<label class="form-label">Lyrics</label>
 								<textarea class="form-control" rows="8" name="lyrics[]" id="song-lyrics"></textarea>
-							</div>
-							<div class="mb-3">
-								<a href="#" class="btn song-action-btn" id="song-more-btn" onclick="addMore()"><i class="bi bi-plus"></i> Add more</a>
-							</div>
-							<div class="more-modal">
-
-							</div>						
+							</div>				
 					</div>
+					<div class="more-modal">
+
+					</div>	
 					<div class="modal-footer">
 						<a href="#" class="btn btn-secondary" data-bs-dismiss="modal" id="song-cancel-btn">Cancel</a>
+						<button type="button" class="btn song-action-btn" id="song-more-btn"><i class="bi bi-plus"></i> Add more</button>
 						<button type="submit" name="update" class="btn btn-warning song-action-btn" id="song-update-btn">Update</a>
 						<button type="submit" name="save" class="btn btn-primary song-action-btn" id="song-save-btn">Save</button>
 					</div>
